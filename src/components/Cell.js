910ec123f91879dbdf910bd668217cacbeb8a95c
dbirlan <daniel.birlan@gmail.com>
2020-11-07 22:17:35 +0100

    Styled cells

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
 
-const Cell = ({ type }) => <div>cell</div>;
+const Cell = ({ type }) => {
+  return (
+    <StyledCell type={'L'} color={TETROMINOS['L'].color}>
+      Cell
+    </StyledCell>
+  );
+};
 
 export default Cell;
