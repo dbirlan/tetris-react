da0949ec316ad206e0b7677302a7434ad361d9bc
dbirlan <daniel.birlan@gmail.com>
2020-11-09 14:37:12 +0100

    player rotation and collision detection added

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -19,7 +19,7 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
   const [stage, setStage] = useStage(player, resetPlayer);
 
   console.log('re-render');
@@ -67,6 +67,9 @@ const Tetris = () => {
         //40 si the keycode for down arrow
       } else if (keyCode === 40) {
         dropPlayer();
+        // 38 is the keycode for up arrow
+      } else if (keyCode === 38) {
+        playerRotate(stage, 1);
       }
     }
   };
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,7 +1,7 @@
 import { useState, useCallback } from 'react';
 
+import { checkCollision, STAGE_WIDTH } from '../gameHelpers';
 import { TETROMINOS, randomTetromino } from '../tetrominos';
-import { STAGE_WIDTH } from '../gameHelpers';
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
@@ -9,6 +9,37 @@ export const usePlayer = () => {
     tetromino: TETROMINOS[0].shape,
     collided: false,
   });
+
+  // function to rotate a matrix
+  const rotate = (matrix, dir) => {
+    // Make the rows become cols (transpose); matrix = tetromino
+    const rotatedTetro = matrix.map((_, index) =>
+      matrix.map((col) => col[index])
+    );
+    // Reverse each row to get a rotated matrix; dir>0 is a clockwise rotation
+    if (dir > 0) return rotatedTetro.map((row) => row.reverse());
+    return rotatedTetro.reverse();
+  };
+
+  const playerRotate = (stage, dir) => {
+    // to avoid mutation on the state, we make a complete clone using JSON.parse(JSON.stringify())
+    const clonedPlayer = JSON.parse(JSON.stringify(player));
+    clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+
+    const pos = clonedPlayer.pos.x;
+    let offset = 1;
+    while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+      clonedPlayer.pos.x += offset;
+      offset = -(offset + (offset > 0 ? 1 : -1));
+      if (offset > clonedPlayer.tetromino[0].length) {
+        rotate(clonedPlayer.tetromino, -dir);
+        clonedPlayer.pos.x = pos;
+        return;
+      }
+    }
+
+    setPlayer(clonedPlayer);
+  };
 
   const updatePlayerPos = ({ x, y, collided }) => {
     setPlayer((prev) => ({
@@ -26,5 +57,10 @@ export const usePlayer = () => {
     });
   }, []);
 
-  return [player, updatePlayerPos, resetPlayer];
+  return [player, updatePlayerPos, resetPlayer, playerRotate];
+  /* const playerState = useState();
+     const player = playerState[0];
+     const setPlayer = playerState[1];
+     => ES6 destructuring syntax is more convenient
+     */
 };
